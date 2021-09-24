e62a9b7c9bd5bb85e6293b33cdd2446721ee1b97
Lev2307 <levchik23072007@gmail.com>
2021-09-24 18:28:24 +0300

    upd

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rating.views import SimpleView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', SimpleView.as_view()),
 ]
